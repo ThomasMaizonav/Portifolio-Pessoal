ce4173f214b2116339eb5f80a8c21a4e3b0cd30a
ThomasMaizonave <139795025+ThomasMaizonav@users.noreply.github.com>
2024-08-14 15:26:52 -0300

    Commit Scrool
    Adicionado o scrool

---
+++ scrool.js
@@ -0,0 +1,24 @@
+const menuLinks = document.querySelectorAll('.right-side a[href^="#"]');
+
+function getDistanceFromTheTop(element) {
+    const id = element.getAttribute("href");
+    const targetElement = document.querySelector(id);
+    return targetElement ? targetElement.offsetTop : 0;
+}
+
+function nativeScroll(distanceFromTheTop) {
+    window.scroll({
+        top: distanceFromTheTop,
+        behavior: "smooth"
+    });
+}
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
+    nativeScroll(distanceFromTheTop);
+}
+
+menuLinks.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+});
